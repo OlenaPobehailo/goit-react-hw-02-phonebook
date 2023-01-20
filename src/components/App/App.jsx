e994da0ec9bde140c6e439e66d7b113fe3e4662d
OlenaPobehailo <olena.pobehailo@gmail.com>
2023-01-20 19:08:19 +0200

    Steps 3, 4

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,11 +1,18 @@
 import { Component } from 'react';
 import { ContactForm } from '../ContactForm/ContactForm';
 import { ContactList } from '../ContactList/ContactList';
+import { Filter } from 'components/Filter/Filter';
 
 import css from './App.module.css';
 export class App extends Component {
   state = {
-    contacts: [],
+    contacts: [
+      { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
+      { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
+      { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
+      { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
+    ],
+    filter: '',
   };
 
   formSubmitHandler = data => {
@@ -18,7 +25,17 @@ export class App extends Component {
     }));
   };
 
+  changeFilter = e => {
+    this.setState({ filter: e.target.value });
+  };
   render() {
+    const { contacts, filter } = this.state;
+
+    const normalizedFilter = filter.toLocaleLowerCase();
+
+    const visibleContacts = contacts.filter(contact =>
+      contact.name.toLocaleLowerCase().includes(normalizedFilter)
+    );
     return (
       <div>
         <h1 className={css.title}>Phonebook</h1>
@@ -27,8 +44,8 @@ export class App extends Component {
           onAddContact={this.addContact}
         />
         <h2 className={css.title}>Contacts</h2>
-
-        <ContactList contacts={this.state.contacts} />
+        <Filter value={filter} onChange={this.changeFilter} />
+        <ContactList contacts={visibleContacts} />
       </div>
     );
   }
---
+++ src/components/Filter/Filter.jsx
@@ -0,0 +1,8 @@
+export const Filter = ({value, onChange}) => {
+  return (
+    <label htmlFor="">
+      Find contacts by name
+      <input type="text" value={value} onChange={onChange} />
+    </label>
+  );
+};

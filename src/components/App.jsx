f28841499ff90dd77c5c19cf9d0e7dcb4ea0adf0
OlenaPobehailo <olena.pobehailo@gmail.com>
2023-10-20 19:25:34 +0300

    adds filter

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -4,7 +4,13 @@ import { Component } from 'react';
 
 export class App extends Component {
   state = {
-    contacts: [],
+    contacts: [
+      { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
+      { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
+      { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
+      { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
+    ],
+    filter: '',
     name: '',
     number: '',
   };
@@ -32,6 +38,10 @@ export class App extends Component {
 
   reset = () => {
     this.setState({ name: '', number: '' });
+  };
+
+  changeFilter = e => {
+    this.setState({ filter: e.target.value });
   };
 
   render() {
@@ -65,11 +75,25 @@ export class App extends Component {
           <button type="submit">Add contact</button>
         </form>
         <h2>Contacts</h2>
-
+        <label htmlFor="">
+          Find contacts by name
+          <input
+            type="text"
+            name="filter"
+            value={this.state.filter}
+            onChange={this.changeFilter}
+          />
+        </label>{' '}
         <ul>
-          {this.state.contacts.map(contact => (
-            <li key={nanoid()}>{contact.name + ': ' + contact.number}</li>
-          ))}
+          {this.state.contacts
+            .filter(contact =>
+              contact.name
+                .toLowerCase()
+                .includes(this.state.filter.toLowerCase())
+            )
+            .map(contact => (
+              <li key={nanoid()}>{contact.name + ': ' + contact.number}</li>
+            ))}
         </ul>
       </div>
     );

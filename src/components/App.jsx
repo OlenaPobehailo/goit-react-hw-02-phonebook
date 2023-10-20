bafb7c09707d4b8ad2f8e1fbbfb998417f5df4d8
OlenaPobehailo <olena.pobehailo@gmail.com>
2023-10-20 18:46:37 +0300

    updates App: adds form and list

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,62 @@
-export const App = () => {
-  return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
-  );
-};
+import { nanoid } from 'nanoid';
+import { Component } from 'react';
+
+export class App extends Component {
+  state = {
+    contacts: [],
+    name: '',
+  };
+
+  handleInputChange = e => {
+    const { name, value } = e.target;
+    console.log(name);
+    console.log(value);
+    this.setState({ [name]: value });
+  };
+
+  handleSubmit = e => {
+    e.preventDefault();
+    // console.log(this.state);
+
+    const newContact = { name: this.state.name };
+    this.setState(prevState => ({
+      contacts: [...prevState.contacts, newContact],
+    }));
+
+    this.reset();
+  };
+
+  reset = () => {
+    this.setState({ name: '' });
+  };
+
+  render() {
+    // console.log(this.state.contacts);
+    return (
+      <div>
+        <h1>Phonebook</h1>
+        <form onSubmit={this.handleSubmit}>
+          <label htmlFor="">
+            Name
+            <input
+              type="text"
+              name="name"
+              value={this.state.name}
+              onChange={this.handleInputChange}
+              required
+            />
+          </label>
+
+          <button type="submit">Add contact</button>
+        </form>
+        <h2>Contacts</h2>
+
+        <ul>
+          {this.state.contacts.map(contact => (
+            <li key={nanoid()}>{contact.name}</li>
+          ))}
+        </ul>
+      </div>
+    );
+  }
+}
